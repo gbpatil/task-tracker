1ba26302ad2c0cecda928ccd1440f226399a558d
Govind Patil <govind.patil@yahooinc.com>
2026-01-07 23:55:30 +0000

    add GITHUB_PAGES env variable in CI build step to enable separate base path

--- vite.config.js
+++ vite.config.js
@@ -7,7 +7,7 @@ const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
 export default defineConfig({
-  base: process.env.GITHUB_ACTIONS ? '/task-tracker/' : '/',
+  base: process.env.GITHUB_PAGES ? '/task-tracker/' : '/',
   plugins: [react()],
   resolve: {
     alias: {
